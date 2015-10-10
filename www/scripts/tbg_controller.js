6caff463aa9578bf65502ef04539ede6ddce74ae
SunilSyal <sunil.syal@gmail.com>
2015-10-10 18:52:57 +0530

    updates

--- www/scripts/tbg_controller.js
+++ www/scripts/tbg_controller.js
@@ -1,7 +1,30 @@
 var TBG = angular.module('TBG', []);
 
-TBG.controller('tbg__main-ctrl', ['tbg__main_service' , function(tbg__main_service) {
+TBG.controller('tbg__main-ctrl', [ 'tbg_loader', 'tbg__main_service', function(tbg_loader, tbg__main_service) {
     var self = this;
+
+    self.config = {
+    	isDev : false,
+    	jsonPath : 'mock/',
+    	serverPath : 'http://thebestgadget.com/TBG/',
+    	siteJsonListFileName : 'siteList.json',
+    	saleJsonListFileName : 'saleList.json'
+    }
+
+    function init(){
+    	setJsonPath();
+    	tbg_loader.loadFile(self.jsonPath + self.config.siteJsonListFileName, proceed);
+    	tbg_loader.loadFile(self.jsonPath + self.config.saleJsonListFileName, proceed);
+    }
+
+    function setJsonPath(){
+    	self.jsonPath = self.config.isDev ? self.config.jsonPath : self.config.serverPath + self.config.jsonPath;
+    }
+
+    function proceed(response){
+    	tbg__main_service.addObject(response);
+    }
+
     self.name ="sunil";
 
     self.openWindow = function (link){
@@ -9,126 +32,18 @@ TBG.controller('tbg__main-ctrl', ['tbg__main_service' , function(tbg__main_servi
     	navigator.app ? navigator.app.loadUrl(link, {openExternal : true}) : window.open(link);
     }
 
+    self.breadcrumb = "Some text here."
+
     self.getSiteList = tbg__main_service.getSiteList;
 
     self.getSaleList = tbg__main_service.getSaleList;
+
+    init();
 }]);
 
 
 TBG.factory('tbg__main_service', function() {
   var objRef = {};
-
-  objRef.siteList = [
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},
-  	{
-  		img: 'images/flipkart.png',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	}
-  ];
-
-  objRef.saleList = [
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Amazon'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'Fashion and Something'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	},
-  	{
-  		date:'Nov 21 - Nov 23',
-  		event: 'Sale',
-  		tgt: "http://www.google.com",
-  		name: 'FlipKart'
-  	}
-  ];
 
   function getSiteList(){
   	return objRef.siteList;
@@ -137,11 +52,53 @@ TBG.factory('tbg__main_service', function() {
   function getSaleList(){
   	return objRef.saleList;
   }
+
+  function addObject(obj){
+  	for(var i in obj){
+  		objRef[i] = obj[i];
+  	}
+
+  	setDate();
+  }
+
+  function setDate(){
+
+  	if(!objRef.saleList) return;
+
+  	var len =  objRef.saleList.length,
+  	today = new Date();
+  	today = today.getTime();
+  	for(var i=0; i< len; i++){
+  		var ref = objRef.saleList[i];
+
+  		if(Date.parse(ref.endDate) < today) {
+  			objRef.saleList[i].date = false;
+  		}else{
+  			ref.date = ref.startDate ? ref.startDate.split(",")[0] + " - " + ref.endDate.split(",")[0] : ref.endDate.split(",")[0];
+  		}
+  	}
+  }
   
   return {
   	getSiteList: getSiteList,
-  	getSaleList : getSaleList
+  	getSaleList : getSaleList,
+  	addObject : addObject
   };
+});
+
+
+TBG.factory('tbg_loader', function($http) {
+
+	function loadFile(fileName, callback){
+		$http.get(fileName).success(function(data) { 
+		    console.log("success!");
+		    callback(data); 
+		});
+	}
+	
+	return {
+	  	loadFile: loadFile
+	};
 });
 
 
