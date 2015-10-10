7801515cd8cd899a4d6207b3d08af31d5dd8ccdd
SunilSyal <sunil.syal@gmail.com>
2015-10-10 10:56:23 +0530

    updates

---
+++ www/scripts/tbg_controller.js
@@ -0,0 +1,43 @@
+var TBG = angular.module('TBG', []);
+
+TBG.controller('tbg__main-ctrl', ['tbg__main_service' , function(tbg__main_service) {
+    var self = this;
+    self.name ="sunil";
+
+    self.openWindow = function (link){
+    	window.open(link);
+    }
+
+    self.getSiteList = tbg__main_service.getSiteList;
+}]);
+
+
+TBG.factory('tbg__main_service', function() {
+  var objRef = [
+  	{
+  		target: "http://www.google.com"
+  	}
+  ];
+
+  function updateLinks(){
+  	for(var i in objRef){
+  		objRef[i].finalTarget = "window.open('"+ objRef[i].target + "','_system', 'location=yes' )"
+  	}
+  }
+
+  function getSiteList(){
+  	return objRef;
+  }
+  
+  updateLinks();
+  return {
+  	getSiteList: getSiteList
+  };
+});
+
+
+TBG.directive('tbg__insert_link', function() {
+  return {
+    template: 'onclick="{{item.finalTarget}}"'
+  };
+});
